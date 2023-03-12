6bf63e135f9f841343f6581e3114a426c4a0c073
kw6rcw9 <120956700+kw6rcw9@users.noreply.github.com>
2023-03-12 23:08:18 +0300

    Made PlayerController

---
+++ 3Dplatformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpPower;
+    private bool _onGround = false;
+
+    void Update()
+    {
+        PlayJump();
+        PlayerMove();
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        _onGround = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        _onGround=false;
+    }
+    private void PlayJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _onGround == true)
+        {
+            _rigidbody.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+           
+        }
+    }
+    
+
+    private void PlayerMove()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        _rigidbody.velocity = new Vector3(z * _speed, _rigidbody.velocity.y, x * -_speed);
+
+    }
+
+}
